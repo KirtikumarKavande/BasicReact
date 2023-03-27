2796097b07358f74caf7a300898c02ac413a7b41
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-03-27 15:20:56 +0530

    delete button

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const ExpenseDetails = (props) => {
+  function deleteHandle (){
+console.log("deleted")
+  }
   return (
     <div>
       <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
-        <div className='expense-item__price'>{props.location}</div>
+        {/* <div className='expense-item__price'>{props.location}</div> */}
+
+        <button onClick={deleteHandle}>delete</button>
     </div>
   )
 }
