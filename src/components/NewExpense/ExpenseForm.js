0cb9c2148555ab9917c00b12483debc97c80e4a1
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-03-31 16:03:23 +0530

    submit button

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,30 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './ExpenseForm.css';
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    const[title,setTitle]=useState("")
-    const[date,setDate]=useState()
-    const[amount,setAmount]=useState(0)
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [amount, setAmount] = useState("");
+function handleSubmit(e){
+e.preventDefault();
+const Data={
+    title,date,amount,
+}
+console.log(Data)
+}
 
   return (
-    <form>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
+    <form onSubmit={handleSubmit}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Title</label>
-          <input type='text' onChange={(e)=>{setTitle(e.target.value)}} value={title} />
+          <input
+            type="text"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            value={title}
+          />
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' onChange={(e)=>{setAmount(e.target.value)}} value={amount}  />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
+            value={amount}
+          />
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' onChange={(e)=>{setDate(e.target.value)}} value={date}  />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+            value={date}
+          />
         </div>
       </div>
-      <div className='new-expense__actions'>
-        <button type='submit'>Add Expense</button>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
